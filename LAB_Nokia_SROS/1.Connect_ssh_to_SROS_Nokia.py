c4e009dd7db9fcb9fc69e1262e299b33d2c2cb71
ducpham85 <phamvanduc85@gmail.com>
2024-01-09 08:44:53 +0700

    changes on 09012024_1

--- LAB_Nokia_SROS/1.Connect_ssh_to_SROS_Nokia.py
+++ LAB_Nokia_SROS/1.Connect_ssh_to_SROS_Nokia.py
@@ -1,26 +1,25 @@
-#1.Import ConnectHander method from the netmiko library
-from netmiko import *
-#2.Declare a divice information to conntect to 
-device={
-    "device_type":"nokia_sros",
-    "ip":"10.18.8.143",
-    "username":"admin",
-    "password":"admin",
-    "port":"9027"
+# 1.Import ConnectHandler method from the netmiko library
+from netmiko import ConnectHandler, NetmikoTimeoutException, NetmikoAuthenticationException
+
+# 2.Declare a device information to connect to
+device = {
+    "device_type": "nokia_sros",
+    "ip": "10.18.8.143",
+    "username": "admin",
+    "password": "admin",
+    "port": "9027"
 }
 try:
-    #3. Connect to device
-    CON=ConnectHandler(**device)
-    #4.Send command to the rouuter
+    # 3. Connect to device
+    CON = ConnectHandler(**device)
+    # 4.Send command to the router
     print(CON.send_command("/admin display-config"))
-    #5.Send config to the router
-    print(CON.send_config_set(["/config router interface test123","address 1.1.1.1/32"]))
-    #6.Disconnect the session
+    # 5.Send config to the router
+    print(CON.send_config_set(["/config router interface test123", "address 1.1.1.1/32"]))
+    # 6.Disconnect the session
     CON.disconnect()
 except NetmikoTimeoutException as e:
     print("Connection Timeout. Please check the hostname, IP, TCP port, firewall, and device availability.")
     print(str(e))
 except NetmikoAuthenticationException:
-    print("Authentication to device failed. Check correct usename/password")
-    
-    
+    print("Authentication to device failed. Check correct username/password")
